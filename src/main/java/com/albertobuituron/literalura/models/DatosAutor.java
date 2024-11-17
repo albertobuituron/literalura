997de372bfe5684e7177032b30f11299384633b7
albertobuituron <alberto.buituron@gmail.com>
2024-11-17 17:15:41 -0300

    Actualizacion por error subida GIT
    Se realizó subida vía transferencia de archivos por error en GIT

---
+++ src/main/java/com/albertobuituron/literalura/models/DatosAutor.java
@@ -0,0 +1,13 @@
+package com.albertobuituron.literalura.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosAutor(
+        @JsonAlias("name") String nombreAutor,
+        @JsonAlias("birth_year") Integer nacimiento,
+        @JsonAlias("death_year") Integer defuncion
+) {
+
+}
